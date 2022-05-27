238cb29420704707fc26c78297aa4dd476b0f376
suriming <soolim520@gmail.com>
2022-05-27 17:41:01 +0900

    frontend init

---
+++ frontend/src/App.js
@@ -0,0 +1,10 @@
+import creatSocketConnection from "./connect";
+
+function App() {
+  // let payload = 0;
+  // payload = creatSocketConnection("ticker");
+  // console.log(payload);
+  return <div></div>;
+}
+
+export default App;
