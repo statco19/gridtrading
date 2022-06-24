18c124c5c0e946574ddd2bd843a1e2d56abcb672
joyjoy00 <eunseo8946@yonsei.ac.kr>
2022-06-24 17:55:41 +0900

    git ignore update

--- src/main/java/com/ybigtaconference/gridtrading/service/UpbitImpl.java
+++ src/main/java/com/ybigtaconference/gridtrading/service/UpbitImpl.java
@@ -85,8 +85,7 @@ public class UpbitImpl implements Upbit {
 //                System.out.println(EntityUtils.toString(entity, "UTF-8"));
                 log.info("commit_order {}", res);
 
-//                System.out.println(EntityUtils.toString(entity, "UTF-8"));
-                log.info("get_balance {}", EntityUtils.toString(entity, "UTF-8"));
+
             } catch (IOException e) {
                 e.printStackTrace();
                 return null;
